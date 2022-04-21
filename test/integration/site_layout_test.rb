28a465ff399d74101ba25df36952cdce1a39eb9c
rkrupnick <rachel@rachelkrupnick.com>
2022-04-21 17:37:41 -0400

    Add layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", static_pages_contact_path
+  end
 end
